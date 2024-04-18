fc07e88b2c3443caecb032e78986a54b6e0ad72b
Heize <dev.heize@gmail.com>
2024-04-18 10:38:04 +0900

    done

--- order/views.py
+++ order/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from seller.models import Food
+from .models import Cart
 
-# Create your views here.
 def order_detail(request, pk):
-    return render(request, 'order/order_detail.html')
+    food = Food.objects.get(pk=pk)
+    context = {
+        'object':food
+    }
+    return render(request, 'order/order_detail.html', context)
+
+from django.http import JsonResponse 
+def modify_cart(request):
+    user = request.user
+    food_id = request.POST['foodId']
+    food = Food.objects.get(pk=food_id)
+    cart, created = Cart.objects.get_or_create(food=food, user=user)
+    cart.amount += request.POST['amountChange']
+    if cart.amount>0:
+        cart.save()
+    context = {
+        'newQuantity':cart.amount,
+        'totalQuantity':cart.amount,
+        'message': '성공',
+        'succese': True,
+    }
+
+    return JsonResponse(context)
